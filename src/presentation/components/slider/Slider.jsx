897032b49b35ddd3bac573df3867a3dbce1d5094
Marta <martalarena2002@gmail.com>
2024-04-04 16:47:46 +0200

    slider works

--- src/presentation/components/slider/Slider.jsx
+++ src/presentation/components/slider/Slider.jsx
@@ -1,13 +1,49 @@
-import Carousel from "react-bootstrap/Carousel";
-import portadaSlider from "../../assets/portadaSlider.png";
-import secondSlider from "../../assets/PORTADA.jpg";
-import thirdSlider from "../../assets/teahupoo-1.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import Carousel from "react-bootstrap/Carousel";
+import portadaSlider from "../../assets/imgSlider1.jpg";
+import secondSlider from "../../assets/imgSlider2.jpg";
+import thirdSlider from "../../assets/imgSlider3.jpg";
 import "./Slider.css";
 
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper/modules";
 export default function Slider() {
   return (
     <>
-      <Carousel className="carousel" data-bs-theme="dark">
+      <div className="relative">
+        <Swiper
+          navigation={true}
+          modules={[Navigation]}
+          className="sliderContainer"
+        >
+          <SwiperSlide>
+            <img
+              src={portadaSlider}
+              alt="Playa de San Juan de la Arena"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={secondSlider}
+              alt="Playa de San Juan de la Arena"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={thirdSlider}
+              alt="Playa de San Juan de la Arena"
+            />
+          </SwiperSlide>
+        </Swiper>
+        <h1 className="sliderTitle">
+          ENCUENTRA LAS MEJORES <br />
+          OPINIONES SOBRE
+          <br /> PLAYAS ASTURIANAS
+        </h1>
+      </div>
+      {/* <Carousel className="carousel" data-bs-theme="dark">
         <Carousel.Item>
           <img
             className="d-block w-100"
@@ -25,7 +61,7 @@ export default function Slider() {
         <Carousel.Item>
           <img className="d-block w-100" src={thirdSlider} alt="Third slide" />
         </Carousel.Item>
-      </Carousel>
+      </Carousel> */}
     </>
   );
 }
