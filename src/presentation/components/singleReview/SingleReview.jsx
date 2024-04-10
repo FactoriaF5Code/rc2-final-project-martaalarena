87e223c8292bfe9d6c0d102a95a853e2a6a014b2
Marta <martalarena2002@gmail.com>
2024-04-10 13:59:39 +0200

    style in single review finish

--- src/presentation/components/singleReview/SingleReview.jsx
+++ src/presentation/components/singleReview/SingleReview.jsx
@@ -3,45 +3,55 @@ import "./SingleReview.css";
 import { useDataContext } from "../../../middleware/context/DataContext";
 
 export default function SingleReview() {
-  
   const { reviewList } = useDataContext();
 
   console.log("Datos de reviewList:", reviewList);
 
   return (
     <section className="totalContainer">
-      {reviewList.map((review) => (
-        <div className="containerImgName" key={review.id}>
-          <img src={review.urlImg} alt="Playa" />
-          <h1>{review.name}</h1>
+      {reviewList.map((review, index) => (
+        <article className="totalContainerInfo" key={index}>
+          <div className="containerImgName" key={review.id}>
+            <img src={review.urlImg} alt="Playa" />
+            <h1>{review.name}</h1>
+          </div>
           <div>
-            <div>
-              <p>Cantidad de gente</p>
-              <ProgressBar now={review.amountOfPeople} />
-            </div>
-            <div>
-              <p>Aparcamiento</p>
-              <ProgressBar now={review.parking} />
-            </div>
-            <div>
-              <p>Dificultad</p>
-              <ProgressBar now={review.dificulty} />
-            </div>
-            <div>
-              <p>Calidad de la ola</p>
-              <ProgressBar now={review.waveCuality} />
-            </div>
-            <div>
-              <p>Accesibilidad</p>
-              <ProgressBar now={review.accessibility} />
-            </div>
-
-            <article>
+            <article className="progressBarContainer">
+              <div>
+                <p>Cantidad de gente</p>
+                <ProgressBar
+                  className="progessBar"
+                  now={review.amountOfPeople}
+                />
+              </div>
+              <div>
+                <p>Aparcamiento</p>
+                <ProgressBar className="progessBar" now={review.parking} />
+              </div>
+              <div>
+                <p>Dificultad</p>
+                <ProgressBar className="progessBar" now={review.dificulty} />
+              </div>
+              <div>
+                <p>Calidad de la ola</p>
+                <ProgressBar className="progessBar" now={review.waveCuality} />
+              </div>
+              <div>
+                <p>Accesibilidad</p>
+                <ProgressBar
+                  className="progessBar"
+                  now={review.accessibility}
+                />
+              </div>
+            </article>
+            <article className="containerDescription">
               <p>Descripción</p>
-              <div>{review.description}</div>
+              <div>
+                <h3>{review.description}</h3>
+              </div>
             </article>
           </div>
-        </div>
+        </article>
       ))}
     </section>
   );
