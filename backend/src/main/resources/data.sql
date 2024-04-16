4c9ec6b2eff8aface8ad0b554404a309e28d5fab
Marta <martalarena2002@gmail.com>
2024-04-16 08:59:12 +0200

    review stars added

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,13 +1,13 @@
 TRUNCATE reviews;
 
 INSERT INTO reviews (name, url_img, parking, description, amount_of_people, dificulty, wave_cuality, accessibility, location) VALUES
-('Playa Norte', 'http://imgfz.com/i/L4YjRuM.jpeg', 100, 'Hermosa playa con olas moderadas, ideal para principiantes y familias.', 50, 20, 30, 40, Ave del Paraiso 7),
-('Playa San Lorenzo', 'http://imgfz.com/i/IuE8qCM.jpeg', 50, 'Olas grandes y fuertes. Recomendado solo para surfistas experimentados.', 30, 50, 50, 20, Gijon ),
-('Aguilar', 'http://imgfz.com/i/4SFzUv7.jpeg', 90, 'Perfecta para aquellos que buscan un dia relajado en el mar sin mucha gente.', 20, 10, 20, 50, Muros del Nalón),
-('Playa del puerto', 'http://imgfz.com/i/FhYVEUc.jpeg', 80, 'Sin estacionamiento. Acceso dificil pero olas perfectas para el surf de alto nivel.', 25, 40, 40, 10, Ribadesella),
-('Los Quebrantos', 'http://imgfz.com/i/aepA9Fi.jpeg', 50, 'Una mezcla de olas para todos los niveles. Buena infraestructura y facil acceso.', 100, 30, 30, 50, San Juan de la Arena),
-('Olas del Sur', 'http://imgfz.com/i/HRXuDwm.jpeg', 70, 'Famosa por sus competiciones de surf. Olas consistentes y desafiantes.', 80, 40, 40, 30, Puerto de Vega),
-('Gulpiyuri', 'http://imgfz.com/i/Eqy6NYi.jpeg', 30, 'Difícil de encontrar, pero ofrece algunas de las mejores olas para surfear en privado.', 20, 30, 50, 20, Arriondas),
-('Playa Clara', 'http://imgfz.com/i/ucfML8a.jpeg', 100, 'Ideal para familias y principiantes. Aguas claras y olas suaves.', 60, 10, 20, 50, Llanes),
-('Playon de Bayas', 'http://imgfz.com/i/nYjxRPw.jpeg', 40, 'Pequeña playa escondida con olas perfectas para surfistas intermedios.', 35, 30, 30, 30, Castrillón),
-('Costa Salvaje', 'http://imgfz.com/i/NhmSuXo.jpeg', 10, 'Acceso extremadamente difícil. Para aventureros en busca de olas vírgenes y desafiantes.', 90, 50, 50, 10, Aviles);
+('Playa Norte', 'http://imgfz.com/i/L4YjRuM.jpeg', 100, 'Hermosa playa con olas moderadas, ideal para principiantes y familias.', 50, 20, 30, 40, 'Ave del Paraiso 7'),
+('Playa San Lorenzo', 'http://imgfz.com/i/IuE8qCM.jpeg', 50, 'Olas grandes y fuertes. Recomendado solo para surfistas experimentados.', 30, 50, 50, 20, 'Gijon' ),
+('Aguilar', 'http://imgfz.com/i/4SFzUv7.jpeg', 90, 'Perfecta para aquellos que buscan un dia relajado en el mar sin mucha gente.', 20, 10, 20, 50, 'Muros del Nalón'),
+('Playa del puerto', 'http://imgfz.com/i/FhYVEUc.jpeg', 80, 'Sin estacionamiento. Acceso dificil pero olas perfectas para el surf de alto nivel.', 25, 40, 40, 10, 'Ribadesella'),
+('Los Quebrantos', 'http://imgfz.com/i/aepA9Fi.jpeg', 50, 'Una mezcla de olas para todos los niveles. Buena infraestructura y facil acceso.', 100, 30, 30, 50, 'San Juan de la Arena'),
+('Olas del Sur', 'http://imgfz.com/i/HRXuDwm.jpeg', 70, 'Famosa por sus competiciones de surf. Olas consistentes y desafiantes.', 80, 40, 40, 30, 'Puerto de Vega'),
+('Gulpiyuri', 'http://imgfz.com/i/Eqy6NYi.jpeg', 30, 'Difícil de encontrar, pero ofrece algunas de las mejores olas para surfear en privado.', 20, 30, 50, 20, 'Arriondas'),
+('Playa Clara', 'http://imgfz.com/i/ucfML8a.jpeg', 100, 'Ideal para familias y principiantes. Aguas claras y olas suaves.', 60, 10, 20, 50, 'Llanes'),
+('Playon de Bayas', 'http://imgfz.com/i/nYjxRPw.jpeg', 40, 'Pequeña playa escondida con olas perfectas para surfistas intermedios.', 35, 30, 30, 30, 'Castrillón'),
+('Costa Salvaje', 'http://imgfz.com/i/NhmSuXo.jpeg', 10, 'Acceso extremadamente difícil. Para aventureros en busca de olas vírgenes y desafiantes.', 90, 50, 50, 10, 'Aviles');
--- src/presentation/components/reviewList/ReviewList.jsx
+++ src/presentation/components/reviewList/ReviewList.jsx
@@ -1,5 +1,7 @@
 import { useDataContext } from "../../../middleware/context/DataContext";
 import { Link } from "react-router-dom";
+import Rating from "@mui/material/Rating";
+import Stack from "@mui/material/Stack";
 import "./ReviewList.css";
 
 // eslint-disable-next-line react/prop-types
@@ -20,6 +22,9 @@ export default function ReviewList({ searchTerm }) {
                 <img src={review.urlImg} alt="playa" />
                 <div>
                   <h1>{review.name}</h1>
+                  <Stack spacing={1}>
+                    <Rating style={{ fontSize: '8rem' }} name="size-" defaultValue={2} size="large" readOnly/>
+                  </Stack>
                   <button onClick={() => handleViewMoreInfo(review.id)}>
                     <Link to={`/review/${review.id}`}>Más info </Link>
                   </button>
